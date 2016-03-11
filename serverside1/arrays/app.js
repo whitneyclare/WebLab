33330acc787f6aa1c29a4b18c43838a530935466
Whitney Todd <whitney1272@gmail.com>
2016-03-10 17:39:42 -0700

    Classwork 3/7 - 3/9

---
+++ serverside1/arrays/app.js
@@ -0,0 +1,9 @@
+var fruits = ['apple', 'kiwi', 'orange', 'strawberry', 'mango', 'kumquat']
+var otherFruit = ['dragon fruit'];
+
+var allFruits = fruits.concat(otherFruit);
+
+console.log(fruits.toString());
+
+
+console.log('JSON verson: ', JSON.stringify(allFruits));
